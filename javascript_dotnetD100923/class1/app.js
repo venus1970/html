3563d16506605bda1100845acda1b9ea5930c036
venus1970 <96591054+venus1970@users.noreply.github.com>
2024-03-16 12:01:33 +0200

    first commit

---
+++ javascript_dotnetD100923/class1/app.js
@@ -0,0 +1,19 @@
+/*console.log("Hello Tzila!");
+var a = 10; /* global variable that can be accessed anywhere in the code and can be changed /// הגדרת המשתנה/ תייצר ותאחסן משתנה /a ושים בתוכו ערך 10*/
+/*var a; //declaire variable
+a=100; //put value in a //תאחסן ערך 100 למשתנה a //אתחול של המשתנה - initialize variable
+b=a+50;//put value in b
+console.log(b);//print b
+/*multiline comments
+multiline comments
+*/
+//one line comment
+
+console.log("Hello");
+let a = 10;
+let b = 20;
+let c = a //c=10, a=10
+a=b//a=20, b=20
+b=c//b=10, c=10
+console.log(a);
+console.log(b);/*
